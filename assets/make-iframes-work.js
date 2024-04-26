01f18c1f0ce93f5cce25ce34ec19c8782606c43e
Michael Greenburg <michael_greenburg@byu.edu>
2024-04-26 11:52:45 -0600

    Links no longer open in parent by default when a page is displayed in an iframe

--- assets/make-iframes-work.js
+++ assets/make-iframes-work.js
@@ -1,14 +1,18 @@
-// Adds `target="_top" to every link on the page and hides the header and footer so that the page can be used by iframes
-// if 'for_iframe=true' is in the link parameters; for use with Canvas.
+/* Hides the header and footer so that the page can be used by iframes if 'for_iframe=true'
+   is in the link parameters; it's meant for use with Canvas. If 'links_open_in_parent=true'
+   is in the link parameters, adds `target="_top" to every link on the page.
+*/
 
 document.addEventListener('DOMContentLoaded', function () {
     const UrlParams = new URLSearchParams(window.location.search);
     if (UrlParams.get('for_iframe') === 'true') {
         // Add target="_top" to links
         var links = document.getElementsByTagName('a');
-        for(var i=0; i<links.length; i++) {
-            if (!links[i].hasAttribute('target')) {
-                links[i].target = '_top';
+        if (UrlParams.get('links_open_in_parent') === 'true') {
+            for(var i=0; i<links.length; i++) {
+                if (!links[i].hasAttribute('target')) {
+                    links[i].target = '_top';
+                }
             }
         }
         // Hide header and footer
